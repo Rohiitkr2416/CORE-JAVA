bdb658166650cb2c2c12d9649daad36658970fbe
Rohiitkr2416 <115978169+Rohiitkr2416@users.noreply.github.com>
2022-10-17 13:18:31 +0530

    Add files via upload

---
+++ 12oct/CLA.java
@@ -0,0 +1,11 @@
+class CLA
+{
+public static void main(String args[])
+{
+System.out.println("Name "+ args[0]);
+System.out.println("Age: "+ args[1]);
+System.out.println("Course "+ args[2]);
+System.out.println("Grade "+ args[3]);
+
+}
+}
